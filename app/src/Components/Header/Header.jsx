9e171d5950441f5e673523d377aca0e2dca0fff3
D-Mastrocola <mastrodomcola@gmail.com>
2023-04-23 11:06:57 -0400

    initial commits

---
+++ app/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+let Header = () => {
+    return (
+        <header>
+            <img src='./assets/ares/header-logo.avif'/>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Swag</a></li>
+            </ul>
+        </header>
+    );
+}
