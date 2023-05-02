5cc72fa733ab1899c21a7e77099b7b9a88399dd4
D-Mastrocola <mastrodomcola@gmail.com>
2023-05-02 13:38:07 -0400

    made team cards modular

--- src/Components/About/Team/Member/Member.jsx
+++ src/Components/About/Team/Member/Member.jsx
@@ -2,29 +2,27 @@ import { Card, Grid } from "@mui/material";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 
-let Member = () => {
+let Member = (props) => {
   return (
     <Grid item xs={3}>
       <Card>
-        <div className="team-card-socials">
-
-        </div>
+        <div className="team-card-socials"></div>
         <CardMedia
           component="img"
-          alt="Dominic Mastrocola"
+          alt={props.name}
           height="140"
-          image="/assets/images/chxpo.png"
+          image={"/assets/images/team/" + props.pfp}
         />
         <CardContent>
-        <Typography gutterBottom variant="body1" component="div">
-          Dominic Mastrocola
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          CEO
-        </Typography>
-      </CardContent>
+          <Typography gutterBottom variant="body1">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.position}
+          </Typography>
+        </CardContent>
       </Card>
     </Grid>
   );
--- src/Components/About/Team/Team.jsx
+++ src/Components/About/Team/Team.jsx
@@ -9,12 +9,17 @@ class Team extends Component {
   render() {
     return (
       <div id="team">
-        <Grid container spacing={2}>
-          <Member />
-          <Member />
-          <Member />
-          <Member />
-          <Member />
+        <Grid container spacing={1}>
+          <Member name='Garrett Leake' position='Owner' pfp='garrett-leake.webp'/>
+          <Member name='Giovanni Tremonte' position='Owner' pfp='giovanni-tremonte.webp'/>
+          <Member name='Chris Carr' position='Owner' pfp='chris-carr.webp'/>
+          <Member name='Thomas King' position='Owner' pfp='thomas-king.webp'/>
+          <Member name='Andrew Perna' position='General Manager' pfp='andrew-perna.webp'/>
+          <Member name='Shayel Khatiwoda' position='Content Manager' pfp='shayel-khatiwoda.webp'/>
+          <Member name='Morgun Hill' position='Community Manager' pfp='morgun-hill.webp'/>  
+          <Member name='Julian Caraballo' position='Social Media Manager' pfp='julian-caraballo.webp'/>  
+          <Member name='Chandler Gilbert' position='Community Manager' pfp='chandler-gilbert.webp'/> 
+          <Member name='Arturo Georg' position='Management' pfp='arturo-georg.webp'/> 
         </Grid>
       </div>
     );
