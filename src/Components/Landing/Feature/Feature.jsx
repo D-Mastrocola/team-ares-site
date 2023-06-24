aa5fd130eb5f3d99da376ab40ac9eb657958b6ac
D-Mastrocola <mastrodomcola@gmail.com>
2023-06-23 22:45:28 -0400

    feature card

--- src/Components/Landing/Feature/Feature.jsx
+++ src/Components/Landing/Feature/Feature.jsx
@@ -1,11 +1,13 @@
 import Typography from "@mui/material/Typography";
+import FeatureCard from "./FeatureCard/FeatureCard";
 
 let Feature = () => {
   return (
     <main>
-      <Typography variant="h2" component="h2">
+      <Typography style={{width: '100%'}}variant="h2" component="h2">
         What's New
       </Typography>
+      <FeatureCard background={'https://publicsite-wordpress-storage-public.insiderintelligence.com/wp-content/uploads/2021/08/03202052/esports-2021-scaled.jpeg'}/>
     </main>
   );
 };
--- src/Components/Landing/Feature/FeatureCard/FeatureCard.jsx
+++ src/Components/Landing/Feature/FeatureCard/FeatureCard.jsx
@@ -1,5 +1,7 @@
-let FeatureCard = () => {
-
+let FeatureCard = ({background}) => {
+    return (
+        <div class='feature-card' style={{backgroundImage: `url(${background})`}}></div>
+    );
 };
 
 export default FeatureCard;
