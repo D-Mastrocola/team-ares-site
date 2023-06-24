df6c34055e3a9d4610eae306731a8dffcfc892be
D-Mastrocola <mastrodomcola@gmail.com>
2023-06-23 23:35:17 -0400

    feature card styling

--- src/Components/Landing/Feature/Feature.jsx
+++ src/Components/Landing/Feature/Feature.jsx
@@ -7,7 +7,7 @@ let Feature = () => {
       <Typography style={{width: '100%'}}variant="h2" component="h2">
         What's New
       </Typography>
-      <FeatureCard background={'https://publicsite-wordpress-storage-public.insiderintelligence.com/wp-content/uploads/2021/08/03202052/esports-2021-scaled.jpeg'}/>
+      <FeatureCard title={'Welcome Morgun Hill'} background={'https://publicsite-wordpress-storage-public.insiderintelligence.com/wp-content/uploads/2021/08/03202052/esports-2021-scaled.jpeg'}/>
     </main>
   );
 };
