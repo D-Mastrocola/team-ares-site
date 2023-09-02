88e24aea945258a2f7c18b9e89345afebaa3b9ae
D-Mastrocola <mastrodomcola@gmail.com>
2023-09-02 16:10:06 -0400

    integrating api

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,7 +1,32 @@
 import Header from "../Header/Header";
 import Animation from "../Animation/Animation";
+import {createStorefrontClient} from '@shopify/hydrogen-react';
+
+const storefront = createStorefrontClient({
+  publicStorefrontToken: process.env.PUBLIC_STOREFRONT_API_TOKEN,
+  storeDomain: `https://${process.env.PUBLIC_STORE_DOMAIN}`,
+  storefrontApiVersion: process.env.PUBLIC_STOREFRONT_API_VERSION || "2023-01",
+});
+
+function loader({ context }) {
+  const PRODUCTS_QUERY = `#graphql
+    query products {
+      products(first: 3) {
+        edges {
+          node {
+            id
+            title
+          }
+        }
+      }
+    }
+  `;
+  const { products } = context.storefront.query(PRODUCTS_QUERY);
+  return JSON.parse({ products });
+}
 
 let Shop = () => {
+  console.log(loader);
   return (
     <>
       <Animation page="about" />
