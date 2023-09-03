9d7c8d352aad7f386d69e5174f3c638c31263a72
D-Mastrocola <mastrodomcola@gmail.com>
2023-09-03 12:37:18 -0400

    before init

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -6,12 +6,12 @@ import {json} from '@shopify/remix-oxygen';
 const storefront = createStorefrontClient({
   publicStorefrontToken: process.env.REACT_APP_PUBLIC_STOREFRONT_API_TOKEN,
   storeDomain: `https://${process.env.REACT_APP_PUBLIC_STORE_DOMAIN}`,
-  storefrontApiVersion: process.env.REACT_APP_PUBLIC_STOREFRONT_API_VERSION || "2023-01",
+  storefrontApiVersion: process.env.REACT_APP_PUBLIC_STOREFRONT_API_VERSION || "2023-07",
 });
 
 
 
-function loader({ context }) {
+function loader( context ) {
   const PRODUCTS_QUERY = `#graphql
     query products {
       products(first: 3) {
@@ -24,17 +24,18 @@ function loader({ context }) {
       }
     }
   `;
-  const { products } = context.storefront.query(PRODUCTS_QUERY);
+  const { products } = storefront.query(PRODUCTS_QUERY);
   return json({products});
 }
 
 let Shop = () => {
-  console.log(loader(storefront));
+  let products = loader();
   return (
     <>
       <Animation page="about" />
       <Header />
       <h1>Shop</h1>
+      <p>{products}</p>
     </>
   );
 };
